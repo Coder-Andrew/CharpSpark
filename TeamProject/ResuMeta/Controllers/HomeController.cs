aff1a214d7e4976b5852a4903405691b12569b5c
Danielle Campbell <dcampbell17@mail.wou.edu>
2024-02-15 19:38:09 -0800

    Added information and images to index and about pages

--- TeamProject/ResuMeta/Controllers/HomeController.cs
+++ TeamProject/ResuMeta/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    [AllowAnonymous]
+    public IActionResult About()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
