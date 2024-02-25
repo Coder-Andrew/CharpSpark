d574f32e5589d2c25d6170c931cf95931d54d456
Ryan Hanson <rhanson19@mail.wou.edu>
2024-02-24 16:19:35 -0800

    Added Quill Editor to project

--- TeamProject/ResuMeta/wwwroot/js/ViewResume.js
+++ TeamProject/ResuMeta/wwwroot/js/ViewResume.js
@@ -1,6 +1,37 @@
-document.addEventListener("DOMContentLoaded", initializePage, false);
+document.addEventListener("DOMContentLoaded", loadQuill, false);
+
+function loadQuill() {
+    console.log("Loading Quill");
+    var script = document.createElement('script');
+    script.src = "https://cdn.quilljs.com/1.0.0/quill.js";
+    document.head.appendChild(script);
+    script.onload = function () {
+        console.log("Quill loaded");
+        initializePage();
+    }
+}
+
 
 function initializePage() {
+    var toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'align': [] }],
+        ['link'],
+        ['clean']                                         // remove formatting button
+    ]
+    var quill = new Quill('#editor', {
+        theme: 'snow',
+        modules: {
+            toolbar: toolbarOptions,
+        }
+    });
+    var htmlContent = document.getElementById('resume-container').outerHTML;
+    const delta = quill.clipboard.convert(htmlContent);
+    quill.setContents(delta);
     const saveBtn = document.getElementById("save-resume");
     saveBtn.addEventListener('click', () => getHtmlInfo(), false);
 }
@@ -8,9 +39,8 @@ function initializePage() {
 async function getHtmlInfo() 
 {
     console.log("Save Resume button clicked");
+    const htmlContent = document.querySelector('.ql-editor').innerHTML;
     const resumeId = document.getElementById('resume-id').value;
-    var htmlContent = document.getElementById('resume-container').outerHTML;
-    console.log(htmlContent);
     const response = await fetch(`/api/resume/${resumeId}`, {
         method: 'PUT',
         headers: {
--- TeamProject/ResuMeta/wwwroot/js/YourResume.js
+++ TeamProject/ResuMeta/wwwroot/js/YourResume.js
@@ -1,7 +1,66 @@
-document.addEventListener("DOMContentLoaded", initializePage, false);
+document.addEventListener("DOMContentLoaded", loadQuill, false);
+
+function loadQuill() {
+    console.log("Loading Quill");
+    var script = document.createElement('script');
+    script.src = "https://cdn.quilljs.com/1.0.0/quill.js";
+    document.head.appendChild(script);
+    script.onload = function () {
+        console.log("Quill loaded");
+        initializePage();
+    }
+}
 
 function initializePage() {
+    var toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'align': [] }],
+        ['link'],
+        ['clean']                                         // remove formatting button
+    ]
+    var quill = new Quill('#editor', {
+        theme: 'snow',
+        modules: {
+            toolbar: toolbarOptions,
+        }
+    });
+    console.log("Editor initialized");
     const resumeContent = document.getElementById("resume-content").value;
     const resumeArea = document.getElementById("resume-area");
     resumeArea.innerHTML = decodeURIComponent(resumeContent);
+    const delta = quill.clipboard.convert(resumeArea.innerHTML);
+    quill.setContents(delta);
+    const saveBtn = document.getElementById("save-resume");
+    saveBtn.addEventListener('click', () => getHtmlInfo(), false);
+}
+
+async function getHtmlInfo() 
+{
+    console.log("Save Resume button clicked");
+    const htmlContent = document.querySelector('.ql-editor').innerHTML;
+    const resumeId = document.getElementById('resume-id').value;
+    const response = await fetch(`/api/resume/${resumeId}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json; application/problem+json; charset=utf-8',
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify({
+            resumeId: resumeId,
+            htmlContent: encodeURIComponent(htmlContent)
+        })
+    });    
+    if (response.ok)
+    {
+        alert('Resume saved successfully.');
+    }
+    else
+    {
+        console.log("Error saving information");
+        alert('An error occurred while saving the resume.');
+    }
 }
