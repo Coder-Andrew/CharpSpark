89de8fe7196e2e4017cee6f9be48212ee48f1004
Coder-Andrew <andrewjesch@gmail.com>
2024-03-01 13:32:45 -0800

    Modify CGPTAPIController

---
+++ TeamProject/ResuMeta/Controllers/ChatGPTAPIController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResuMeta.Models;
+using ResuMeta.Services.Abstract;
+
+namespace ResuMeta.Controllers
+{
+    [Route("api/cgpt")]
+    [ApiController]
+    public class ChatGPTAPIController : ControllerBase
+    {
+        private readonly IChatGPTService _chatGPTService;
+        public ChatGPTAPIController(IChatGPTService chatGPTService)
+        {
+            _chatGPTService = chatGPTService;
+        }
+
+        // POST: api/cgpt/ask
+        [HttpPost("ask")]
+        [ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ChatGPTResponse>> AskQuestion([FromBody] string question)
+        {
+            try
+            {
+                var response = await _chatGPTService.AskQuestion(question);
+                return Ok(response);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
