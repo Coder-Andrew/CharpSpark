770cff37350db18af2080686d04fd4580e952f30
zoerobb <zoeannrobb@gmail.com>
2024-03-10 19:57:55 -0700

    Set up the cover letter controller, index page, and some placeholder content in the form page. Added cover letters to the navbar.

---
+++ TeamProject/ResuMeta/Controllers/CoverLetterController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ResuMeta.Models;
+using ResuMeta.DAL.Abstract;
+using Microsoft.AspNetCore.Identity;
+using ResuMeta.ViewModels;
+using System.Linq;
+using ResuMeta.Services.Concrete;
+using ResuMeta.Services.Abstract;
+using ResuMeta.Data;
+
+namespace ResuMeta.Controllers;
+
+public class CoverLetterController : Controller
+{
+    private readonly ILogger<ResumeController> _logger;
+    private readonly IRepository<UserInfo> _userInfo;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public CoverLetterController(
+        ILogger<ResumeController> logger,
+        IRepository<UserInfo> userInfo,
+        UserManager<ApplicationUser> userManager
+        )
+    {
+        _logger = logger;
+        _userInfo = userInfo;
+        _userManager = userManager;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult CreateCoverLetter()
+    {
+        return View();
+    }
+}
