9719f0964e6d8912562a958ae0aff995ad71bcc8
zoerobb <zoeannrobb@gmail.com>
2024-04-05 20:03:12 -0700

    Updated cover letter controllers, services, and repositories to have basic functionality for getting all cover letters and saving cover letters, as well as added export and save button to ViewCoverLetter.cshtml

--- TeamProject/ResuMeta/ViewModels/CoverLetterVM.cs
+++ TeamProject/ResuMeta/ViewModels/CoverLetterVM.cs
@@ -2,9 +2,9 @@ namespace ResuMeta.ViewModels
 {
     public class CoverLetterVM
     {
-        public int? UserInfoId { get; set; }
-        public string? Title { get; set; }
         public int? CoverLetterId { get; set; }
+        public string? Title { get; set; }
+        public string? HtmlContent { get; set; }
         public string? HiringManager { get; set; }
         public string? Body { get; set; }
     }
