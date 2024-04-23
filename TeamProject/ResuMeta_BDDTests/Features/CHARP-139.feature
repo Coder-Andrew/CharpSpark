f97c77516603bf0660077b01abf42d634e32d906
Danielle Campbell <dcampbell17@mail.wou.edu>
2024-04-22 19:31:24 -0700

    Finished BDD testing, all tests pass

--- TeamProject/ResuMeta_BDDTests/Features/CHARP-139.feature
+++ TeamProject/ResuMeta_BDDTests/Features/CHARP-139.feature
@@ -20,7 +20,7 @@ Scenario: User visits the application tracker page
 Given I am a user with the first name 'Adrian'
     And I login
     And I am on the "ApplicationTracker" page
-Then I will be able to see a table one entry that tells me I have no application trackers
+Then I will be able to see a table with one entry that tells me I have no application trackers
 
 Scenario: User adds job to application tracker
 Given I am a user with the first name 'Adrian'
@@ -29,10 +29,16 @@ Given I am a user with the first name 'Adrian'
 When I submit my information in the ApplicationTracker page form
 Then I will be able to see a table row containing my ApplicationTracker page form response
 
-Scenario: User adds job to application tracker
+Scenario: User ensures that the jobs are in ascending order of application deadline
 Given I am a user with the first name 'Adrian'
     And I login
     And I am on the "ApplicationTracker" page
-    And I can see my jobs on the ApplicationTracker page form
 When I filter my applications by application deadline ascending
 Then I will be able to see the jobs in my table in ascending order of application deadline
+
+Scenario: User deletes job from application tracker
+Given I am a user with the first name 'Adrian'
+    And I login
+    And I am on the "ApplicationTracker" page
+When I click on the trash can icon next to an entry on my ApplicationTracker table
+Then I will not be able to see that entry in my ApplicationTracker table
--- TeamProject/ResuMeta_BDDTests/StepDefinitions/CHARP-139StepDefinitions.cs
+++ TeamProject/ResuMeta_BDDTests/StepDefinitions/CHARP-139StepDefinitions.cs
@@ -13,18 +13,15 @@ namespace ResuMeta_BDDTests.StepDefinitions
     {
         private readonly ScenarioContext _scenarioContext;
         private readonly ApplicationTrackerPageObject _applicationTrackerPage;
-        private readonly ILogger<ApplicationTrackerPageObject> _logger;
-        public CHARP139StepDefinitions(ScenarioContext scenarioContext, BrowserDriver browserDriver, ILogger<ApplicationTrackerPageObject> logger)
+        public CHARP139StepDefinitions(ScenarioContext scenarioContext, BrowserDriver browserDriver)
         {
-            _logger = logger;
             _scenarioContext = scenarioContext;
-            _applicationTrackerPage = new ApplicationTrackerPageObject(browserDriver.Current, _logger);
+            _applicationTrackerPage = new ApplicationTrackerPageObject(browserDriver.Current);
         }
 
-        [Then(" Then I will be able to see a table with one entries")]
-        public void ThenIWillBeAbleToSeeATableWithOneEntry()
+        [Then("I will be able to see a table with one entry that tells me I have no application trackers")]
+        public void ThenIWillBeAbleToSeeATableWithOneEntryThatTellsMeIHaveNoApplicationTrackers()
         {
-            _logger.LogInformation("I am working :)");
             _applicationTrackerPage.CheckIfTableHasOneEntry();
         }
         [When("I submit my information in the Application Tracker form")]
@@ -35,23 +32,44 @@ namespace ResuMeta_BDDTests.StepDefinitions
             _applicationTrackerPage.SubmitForm();
         }
 
-        [Then("Then the new job application should be added to my application tracker table")]
-        public void ThenTheNewJobApplicationShouldBeAddedToMyApplicationTrackerTable()
+        [Then("I will be able to see a table row containing my ApplicationTracker page form response")]
+        public void ThenIWillBeAbleToSeeATableRowContainingMyApplicationTrackerPageFormResponse()
         {
             _applicationTrackerPage.IsJobApplicationAddedToTable().Should().BeTrue();
         }
 
-        // [When("When I filter my applications by application deadline ascending")]
-        // public void WhenIFilterMyApplicationsByApplicationDeadlineAscending()
-        // {
-        //     _applicationTrackerPage.SortBy();
-        // }
+        [When("I filter my applications by application deadline ascending")]
+        public void WhenIFilterMyApplicationsByApplicationDeadlineAscending()
+        {
+            _applicationTrackerPage.FillOutForm();
+            _applicationTrackerPage.FilterByApplicationDeadlineAscending();
+        }
 
-        // [Then("Then I will be able to see the jobs in my table in ascending order of application deadline")]
-        // public void ThenIWillBeAbleToSeeTheJobsInMyTableInAscendingOrderOfApplicationDeadline()
-        // {
-        //     _applicationTrackerPage.AreJobsSorted().Should().BeTrue();
-        // }
+        [Then("I will be able to see the jobs in my table in ascending order of application deadline")]
+        public void ThenIWillBeAbleToSeeTheJobsInMyTableInAscendingOrderOfApplicationDeadline()
+        {
+            _applicationTrackerPage.AreJobsSorted().Should().BeTrue();
+        }
 
+        [When("I submit my information in the ApplicationTracker page form")]
+        public void WhenISubmitMyInformationInTheApplicationTrackerPageForm()
+        {
+            _applicationTrackerPage.FillOutForm();
+            _applicationTrackerPage.SubmitForm();
+        }
+
+        [When("I click on the trash can icon next to an entry on my ApplicationTracker table")]
+        public void WhenIClickOnTheTrashCanIconNextToAnEntryOnMyApplicationTrackerTable()
+        {
+            _applicationTrackerPage.FillOutFormForDeleteButoon();
+            _applicationTrackerPage.SubmitForm();
+             _applicationTrackerPage.DeleteJobApplication();
+        }
+
+        [Then("I will not be able to see that entry in my ApplicationTracker table")]
+        public void ThenIWillNotBeAbleToSeeThatEntryInMyApplicationTrackerTable()
+        {
+            _applicationTrackerPage.IsJobApplicationDeleted().Should().BeTrue();
+        }
     }
 }
