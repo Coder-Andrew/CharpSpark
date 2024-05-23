b544fb5040111321a34ef9a864e32402b918595d
zoerobb <zoeannrobb@gmail.com>
2024-05-22 21:55:02 -0700

    Finished cookie authentication

--- TeamProject/ResuMeta/wwwroot/js/site.js
+++ TeamProject/ResuMeta/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", mainInitializePage, false);
 
-// Write your JavaScript code.
+function mainInitializePage() {
+    var consentModal = document.getElementById('cookie-consent-modal');
+    var isAuthenticated = document.getElementById('is-authenticated').value === 'true';
+    
+    if (isAuthenticated || localStorage.getItem('cookiesAccepted') === 'true') {
+        return;
+    }
+
+    consentModal.style.display = 'block';
+
+    var declineModal = document.getElementById('decline-modal');
+    var consentYes = document.getElementById('consent-yes');
+    var consentNo = document.getElementById('consent-no');
+    consentModal.style.display = 'block';
+    consentYes.addEventListener('click', () => acceptCookie(consentModal), false);
+    consentNo.addEventListener('click', () => declineCookie(declineModal, consentModal), false);
+}
+
+function acceptCookie(consentModal) {
+    consentModal.style.display = 'none';
+    localStorage.setItem('cookiesAccepted', 'true');
+    console.log('Cookies accepted: ' + localStorage.getItem('cookiesAccepted'));
+}
+
+function declineCookie(declineModal, consentModal) {
+    declineModal.style.display = 'block';
+    consentModal.style.display = 'none';
+}
